435a59469fc558c6176d921b6b0498cfc7ca14e5
NextGeneration22 <ayoubejraidi@gmail.com>
2024-08-29 15:44:10 +0100

    Add files via upload

---
+++ everyUseLoop.js
@@ -0,0 +1,8 @@
+const x = [2,4,6,8,9,10];
+for(i=0; i<x.length; i++){
+    let y = x[i]%2;
+    if(y != 0){console.log("fals")}
+    else{console.log("true")}   
+}
+// creat every use for loop.
+// rise the next chalenge try to print only one "true" if the all the output are true, and one "fals" if an output is fals.
